f04c3ebb9202b432b2e3ffacd456d4804f6628d0
burhan <smdburhan25@gmail.com>
2022-08-25 15:05:43 +0530

    learn about function

--- Coding-B-Task/fundamental_exercise.js
+++ Coding-B-Task/fundamental_exercise.js
@@ -5,9 +5,9 @@ function printingString(str) {
     console.log(str);
   }
 }
-// printingString("hello");
-// printingString("buhari");
-// printingString("burhan");
+printingString("hello");
+printingString("buhari");
+printingString("burhan");
 
 // task 2: print 5 times each
 
@@ -25,7 +25,7 @@ function printStringWithNames(str) {
     }
   }
 }
-// printStringWithNames("hello");
+printStringWithNames("hello");
 
 // task 3: finding odd numbers
 
@@ -63,52 +63,59 @@ function findEvenNumber(arrayOfNumbers) {
 }
 
 var numb = [20, 19, 15, 16, 8, 4, 6, 9];
-// findEvenNumber(numb);
+findEvenNumber(numb);
 
 // task 5: multiply of 9
 
 function CheckMultiplyByNine(ninth) {
-  for (var i = 0; i < ninth.length; i++) {
-    if (ninth[i] % 9 == 0) {
-      console.log(`${ninth[i]} is multiply by nine`);
+  for (var i = 2; i <= ninth; i++) {
+    if (i % 9 == 0) {
+      console.log(`${i} is multiply by nine`);
     }
   }
 }
-var ninth = [
-  8, 9, 17, 18, 26, 27, 33, 36, 44, 100, 98, 2, 45, 55, 54, 63, 62, 72, 77, 81,
-  90,
-];
-// CheckMultiplyByNine(ninth);
+
+CheckMultiplyByNine(90);
 
 //------------------------------------------------------------------
 
 // task 6:finding even or odd nummers in ninth tables
 
 function multiplybyninth(ninth) {
-  for (var i = 0; i < ninth.length; i++) {
-    if (ninth[i] % 2 == 0) {
-      console.log(`${ninth[i]} is an even number`);
-    } else {
-      console.log(`${ninth[i]} is an odd number`);
+  for (var i = 2; i <= ninth; i++) {
+    if (i % 9 == 0 && i % 2 == 0) {
+      console.log(`${i} is an even number`);
     }
+    // else {
+    //   console.log(`${i} is an odd number`);
+    // }
   }
 }
 
-multiplybyninth(ninth);
+multiplybyninth(90);
 
 //------------------------------------------------------------------------------
 
-// var x = 25;
-// x += 10;
-// console.log(x);
+function reverseanumber(a) {
+  var reverse = a.split("");
+  var reverses = reverse.reverse();
+  console.log(`The given number is reverse ${reverses}`);
+}
+// var revnumber = "123456789";
+reverseanumber("123456789");
 
-// function rev(num) {
-//   for (var i = num.length; i >= 0; i--) {
-//     console.log(num[i]);
-//   }
-// }
-// var number = "987654321";
-// rev(number);
+const arry = [
+  "apple",
+  "apple",
+  "jack",
+  "mango",
+  "jack",
+  "kiwi",
+  "lemon",
+  "mango",
+];
 
-// var v = false;
-// console.log(Number(v));
+const toFindDuplicates = (arry) =>
+  arry.filter((a, b) => arry.indexOf(a) !== b);
+const duplicateElements = toFindDuplicates(arry);
+console.log(duplicateElements);
