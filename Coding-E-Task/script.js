ef690fef7ba5907008a5b788112641a6c911ea92
burhan <smdburhan25@gmail.com>
2022-08-29 13:01:32 +0530

    new file

--- Coding-E-Task/script.js
+++ Coding-E-Task/script.js
@@ -45,57 +45,6 @@ function printEvenNumbersWithInLimit(endNumber) {
 }
 
 printEvenNumbersWithInLimit(50);
-// task:4
-
-var filelist = [
-  "file1.pdf",
-  "file2.docx",
-  "file3.mp3",
-  "file4.docx",
-  "file5.pdf",
-  "file6.png",
-];
-var extension1 = ".docx";
-function findFiles(a, b) {
-  //arugement
-  var result = a.filter(function (files) {
-    //console.log(result);
-    return files.endsWith(b);
-  });
-  return result;
-}
-
-var number1 = 50;
-
-var largeFilelist = [
-  "file1.pdf",
-  "file2.docx",
-  "file3.mp3",
-  "file4.docx",
-  "file5.pdf",
-  "file6.png",
-  "file1.pdf",
-  "file2.docx",
-  "file3.mp3",
-  "file4.docx",
-  "file5.pdf",
-  "file6.png",
-  "file1.pdf",
-  "file2.docx",
-  "file3.mp3",
-  "file4.docx",
-  "file5.exe",
-  "file6.exe",
-];
-var extensionStr = ".exe";
-var extension = ".png";
-var resultResponse = findFiles(largeFilelist, extensionStr);
-var resultResponses = findFiles(filelist, extension1); //parameter
-var resultResponse1 = findFiles(filelist, extension);
-console.log(resultResponse);
-console.log(resultResponses);
-console.log(resultResponse1);
-
 // task:5
 
 var number = [1, 2, 3, 4, 5];
@@ -207,49 +156,31 @@ console.log(arrange);
 var joinAscending = ascending.join("-");
 console.log(joinAscending);
 
-const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+// Array.prototype.myFilter = function (cb) {
+//   var array = this;
+//   var finalresult = [];
+//   for (i = 0; i < array.length; i++) {
+//     var result2 = cb(array[i], i, array);
+//     if (result2) {
+//       finalresult.push(array[i]);
+//     }
+//   }
+//   return finalresult;
+// };
+// const filteredNumbers = numbers.filter(function (value, index, myArray) {
+//   return value < 5;
+// });
 
-const filteredNumbers = numbers.filter(function (value, index, myArray) {
-  return value > 5;
-});
+// console.log(filteredNumbers);
 
-console.log(filteredNumbers);
+// const filteredNumbers1 = numbers.myFilter(function (value, index, myArray) {
+//   return value > 7;
+// });
 
-const filteredNumbers1 = numbers.myFilter(function (value, index, myArray) {
-  return value > 5;
-});
+// console.log(filteredNumbers1);
 
-console.log(filteredNumbers1);
 
-// function MyArray() {}
-// MyArray.filter = function() {};
-// var myArr = new MyArray();
-// myArr.filter((a) => a > 5);
 
-// var finalresult = [];
-const result1 = myFilter(function (value, index, numbers) {
-  return value > 5;
-}, numbers);
-
-// OutPut [ 6, 7, 8, 9 ]
-console.log(result1);
-
-// how to write own filter
-let items = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
-let isEven = function (item) {
-  return item % 2 === 0;
-};
-console.log(isEven);
-const result = [];
-
-for (let i = 0; i < items.length; i++) {
-  if (isEven(items[i])) {
-    result.push(items[i]);
-  }
-}
-
-console.log(result);
-// Result: [2, 4, 6, 8, 10]
 
 // Constructor function for Person objects
 function Person(first, last, age, eye) {
