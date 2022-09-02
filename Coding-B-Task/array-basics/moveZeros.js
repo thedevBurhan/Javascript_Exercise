39d5c0b430a51f65858a8bedfbdaff04447d8238
buharisahith <buharisahith@gmail.com>
2022-09-02 10:12:07 +0530

    Added QA in arrat-basics

---
+++ Coding-B-Task/array-basics/moveZeros.js
@@ -0,0 +1,14 @@
+/**
+ * Write a function that should found the 0 and move to the last .
+ * @param [Numbers] 
+ * @return [Numbers]
+ * 
+ * Input - [0, 1, 2, 0, 100, 0, 0, 0, 9, 5 , 0, 7, 4, 0 ]
+ * Output Ans - [1, 2, 100, 9, 5, 7, 4, 0, 0, 0, 0, 0, 0, 0]
+ * 
+ * Note :
+ * - Use looping , conditionals
+ * - You can use array methods
+ */
+
+
