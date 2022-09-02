5b7b1ddb5494ababac868ae632c8fe6ce6aeb75b
burhan <smdburhan25@gmail.com>
2022-09-02 17:09:08 +0530

    learned and completed (to find task)

--- Coding-B-Task/array-basics/findMax.js
+++ Coding-B-Task/array-basics/findMax.js
@@ -45,16 +45,17 @@ for (i = 0; l > i; i++) {
 console.log(max);
 
 // type :5
-var array = [3, 6, 2, 56, 32, 5, 89, 32];
-var largest = 0;
-
-for (i = 0; i <= largest; i++) {
-  if (array[i] > largest) {
-    var largest = array[i];
+var array = [3, 6, 2, 1025, 56, 32, 5, 89, 32];
+function findMax(array) {
+  var largest = 0;
+  for (i = 0; i <= largest; i++) {
+    if (array[i] > largest) {
+      var largest = array[i];
+    }
   }
+  return largest;
 }
-
-console.log(largest);
+var result = findMax(array);
+console.log(result);
 
 // type:6
-
