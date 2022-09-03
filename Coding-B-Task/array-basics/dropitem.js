26d7268ebb17c7ee3bbe5c15530f69298c7ffc36
burhan <smdburhan25@gmail.com>
2022-09-03 19:48:52 +0530

    Implemented array slice prototype

--- Coding-B-Task/array-basics/dropitem.js
+++ Coding-B-Task/array-basics/dropitem.js
@@ -1,11 +1,22 @@
 /**
  * Task description: Write a method to creates a slice of array with n elements dropped from the beginning.
  * Expected Result: [1, 2, 3, 4, 5, 6] => [3, 4, 5, 6]
- * 
+ * Expected Result: [1, 2, 3, 4, 5, 6] => [5, 6]
+ *
  * @param {Array} array - Array of any elements
  * @param {Number} elementToDrop - number of elements to drop.
  * @returns {Array}
  * Don't use slice method !!
  */
 
-
+// type:1
+var number = [1, 2, 3, 4, 5, 6];
+function elementToDrop(number, elementToDrop) {
+  var remainingNums = [];
+  for (i = elementToDrop; i < number.length; i++) {
+    remainingNums.push(number[i]);
+  }
+  return remainingNums;
+}
+console.log(elementToDrop(number, 2));
+console.log(elementToDrop(number , 4))
