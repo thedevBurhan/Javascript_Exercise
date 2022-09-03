c9897bf5ea54b4eb6522af72348d6ccd09d4a890
buharisahith <buharisahith@gmail.com>
2022-09-03 16:33:42 +0530

    Added array slice task

---
+++ Coding-B-Task/array-basics/dropitem.js
@@ -0,0 +1,11 @@
+/**
+ * Task description: Write a method to creates a slice of array with n elements dropped from the beginning.
+ * Expected Result: [1, 2, 3, 4, 5, 6] => [3, 4, 5, 6]
+ * 
+ * @param {Array} array - Array of any elements
+ * @param {Number} elementToDrop - number of elements to drop.
+ * @returns {Array}
+ * Don't use slice method !!
+ */
+
+
