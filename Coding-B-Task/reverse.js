ccf10d38258500e35d4228938eb2759faed0d396
burhan <smdburhan25@gmail.com>
2022-09-04 22:02:23 +0530

    Learned and completed task

--- Coding-B-Task/reverse.js
+++ Coding-B-Task/reverse.js
@@ -1,5 +1,16 @@
 /**
- * Task description: Write a function to reverse a given item 
+ * Task description: Write a function to reverse a given item
  * Note : should not use reverse method
  */
 
+function reverseStr(str) {
+  var strng = str.toString();
+  var result = "";
+  for (i = strng.length - 1; i >= 0; i--) {
+    // result = result + strng[i]; 
+    result += strng[i]
+  }
+  return result;
+}
+console.log(reverseStr("ILOVEPROGRAMMING"));
+console.log(reverseStr(123456789));
