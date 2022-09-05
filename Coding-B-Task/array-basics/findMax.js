282b8fcd962ca07fc0f8d81ea15a80e0137014a7
burhan <smdburhan25@gmail.com>
2022-09-05 18:41:01 +0530

    tried to find the minimum value

--- Coding-B-Task/array-basics/findMax.js
+++ Coding-B-Task/array-basics/findMax.js
@@ -9,53 +9,63 @@
  */
 
 // type :1
-// var number = [1, 2, 3, 4, 5, 6, 7, 8, 9, 100];
-// var max = Math.max(number);
-// for (i = 0; i < number.length; i++) {
-//   if (number[i] < max) {
-//     console.log(`less then max value ${number[i]}`);
-//   } else {
-//     console.log(`The Maximun value is ${number[i]}`);
-//   }
-// }
+var number = [1, 2, 3, 4, 5, 6, 7, 8, 9, 100];
+var max = Math.max(number);
+for (i = 0; i < number.length; i++) {
+  if (number[i] < max) {
+    console.log(`less then max value ${number[i]}`);
+  } else {
+    console.log(`The Maximun value is ${number[i]}`);
+  }
+}
 
 // type:2
 
-// var max = number[9];
-// for (i = 0; i < number.length; i++) {
-//   if (number[i] < max) {
-//     console.log(`less then max value ${number[i]}`);
-//   } else {
-//     console.log(`The Maximun value is ${number[i]}`);
-//   }
-// }
-//
+var max = number[9];
+for (i = 0; i < number.length; i++) {
+  if (number[i] < max) {
+    console.log(`less then max value ${number[i]}`);
+  } else {
+    console.log(`The Maximun value is ${number[i]}`);
+  }
+}
 
 // type:3
 
-// var Numbers = [1, 2, 101, 45, 55, 1443];
-// var l = Numbers.length;
-// var max = -Infinity;
-// for (i = 0; l > i; i++) {
-//   if (Numbers[i] > max) {
-//     max = Numbers[i];
-//   }
-// }
+var Numbers = [1, 2, 101, 45, 55, 1443];
+var l = Numbers.length;
+var max = -Infinity;
+for (i = 0; l > i; i++) {
+  if (Numbers[i] > max) {
+    max = Numbers[i];
+  }
+}
 
 // console.log(max);
 
 // // type :5
-// var array = [3, 6, 2, 1025, 56, 32, 5, 89, 32];
-// function findMax(array) {
-//   var largest = 0;
-//   for (i = 0; i <= largest; i++) {
-//     if (array[i] > largest) {
-//       var largest = array[i];
-//     }
-//   }
-//   return largest;
-// }
-// var result = findMax(array);
-// console.log(result);
+var array = [30, 60, 20, 56,  32, 89, 32, 2, 300,1];
+function findMax(array) {
+  var largest = 0;
+  for (i = 0; i <= largest; i++) {
+    if (array[i] > largest) {
+      var largest = array[i];
+    }
+  }
+  return largest;
+}
+var result = findMax(array);
+console.log(result);
 
-// type:6
+// type:6 find min
+function findMini(array) {
+  var largest = 500;
+  for (i = 0; i <= largest; i++) {
+    if (array[i] < largest) {
+      var largest = array[i];
+    }
+  }
+  return largest;
+}
+var result = findMini(array);
+console.log(result);
