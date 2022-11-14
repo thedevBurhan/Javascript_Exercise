15347e5b8826a92ebcd83d851617511f102ea2ce
thedevBurhan <buharisahith@gmail.com>
2022-11-14 14:06:44 +0530

    group by key and value task

---
+++ Coding-B-Task/array-basics/groupBy.js
@@ -0,0 +1,93 @@
+var values =Object.values(employees);
+  var newObject=[];
+  for (i = 0; i <= employees.Length; i++) {
+    if (values[i] == Tester) {
+       newObject.push (values[i]);
+    }
+ console.log 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+const employees = [
+  {
+    name: "buhari",
+    batchNumber: "B1",
+    department: "Tester",
+    designation: "Senior Software Developer",
+    age: "25",
+    gender: "male"
+  },
+  {
+    name: "eliyas",
+    batchNumber: "B1",
+    department: "IT",
+    designation: "Director",
+    age: "29",
+    gender: "male"
+  },
+  {
+    name: "burhan",
+    batchNumber: "B2",
+    department: "IT",
+    designation: "Tester",
+    age: "22",
+    gender: "male"
+  },
+  {
+    name: "vishak",
+    batchNumber: "B3",
+    department: "IT",
+    designation: "Assosiate Developer",
+    age: "25",
+    gender: "male"
+  },
+  {
+    name: "revi",
+    batchNumber: "B4",
+    department: "UX",
+    designation: "Intern",
+    age: "20",
+    gender: "male"
+  },
+  {
+    name: "nikitha",
+    batchNumber: "B3",
+    department: "UX",
+    designation: "Intern",
+    age: "21",
+    gender: "Female"
+  },
+  {
+    name: "pradeep",
+    batchNumber: "B2",
+    department: "Cyber Security",
+    designation: "Senior Software Developer",
+    age: "25",
+    gender: "Femmale"
+  },
+];
+
+
+// Tester : [  {
+//     name: "buhari",
+//     batchNumber: "B1",
+//     department: "Tester",
+//     designation: "Senior Software Developer",
+//     age: "25",
+//     gender: "male"
+//   },
+
+// B2: [{
+    
+// }]]
